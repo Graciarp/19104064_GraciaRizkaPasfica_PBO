51dbddbf74387bd1add5e631895b51a91d6b88d5
Graciarp <graciariska1262@gmail.com>
2020-10-26 14:30:09 +0700

    Pertemuan 1

---
+++ PraktikumPBO/src/com/gracia/Pertemuan_1/Praktikum/manusia/manusia.java
@@ -0,0 +1,41 @@
+package com.gracia.Pertemuan_1.Praktikum.manusia;
+
+public class manusia {
+    private String nama;
+    private int umur;
+    /*
+        Pada OOP biasakan untuk menggunakan modifier private
+        untuk constructor. Jika menggunakan modifier public
+        maka class lain dapat memanggil vairable tersebut
+     */
+
+    //Constructor Pertama (tanpa parameter)
+    public manusia(){}
+
+    //Constructor dengan parameter
+    public manusia(String nama){
+        this.nama = nama;
+        //this merujuk pada class
+        //memperjelas bahwa nama ada di class manusia
+    }
+
+    //Constructor dengan parameter
+    public manusia(String nama, int umur){
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    //Getter & Setter
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public int getUmur(){
+        return umur;
+    }
+    public void setUmur(int umur){
+        this.umur = umur;
+    }
+}
